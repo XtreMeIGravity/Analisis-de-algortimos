4fdeb666ab5d756421d81fca1c03782776fbbb71
XtreMeIGravity <xtremeigraviti@hotmail.com>
2020-02-03 22:24:47 -0600

    commit

--- Practica1_2/practica1_2.c
+++ Practica1_2/practica1_2.c
@@ -1,12 +1,52 @@
-#include <stdio.h>
+/*
+        INSTITUTO POLITÉCNICO NACIONAL
+         ESCUELA SUPERIOR DE CÓMPUTO
+           ANÁLISIS DE ALGORITMOS 
+              SEMESTRE 2020-2
+                GRUPO: 3CV2
+        EQUIPO:
+          -- LÓPEZ HERNÁNDEZ DAVID
+          -- TOLENTINO PÉREZ JAZMIN YANELI
+             PRÁCTICA No1: DETERMINACIÓN EXPERIMENTAL
+            DE LA COMPLEJIDAD TEMPORAL DE UN ALGORITMO
+*/
 
-void main(){
-}
-int euclides(int *m, int *n){
-    while (n != 0){
-        r=m%n;
-        m=n;
-        n=r;
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int mcd(int m, int n);
+int fibonacci(int n);
+
+int main(){
+ int n,m,i,lim=10;
+    for(i=1; i<=lim; i++){
+      m=fibonacci(i);
+      n=fibonacci(i-1);
+      printf("\nN: %d, M=%d \n",n,m);
+      printf("MCD: %d \n",mcd(m,n));
     }
-    return m;    
+    return 0;
+}
+
+int mcd(int m, int n){
+    int r=0, cont=1;
+    while(n!=0){ cont++;
+        r= m%n; cont++;
+        m=n; cont++;
+        n=r;cont++;
+    }cont++;
+    printf("Tiempo: %d\n",cont);
+    return m;
+}
+
+int fibonacci(int n){
+  if (n>2)
+    return fibonacci(n-1) + fibonacci(n-2);
+  else if (n==2)
+    return 1;
+  else if (n==1)       
+    return 1;
+  else if (n==0)
+    return 0;
 }
